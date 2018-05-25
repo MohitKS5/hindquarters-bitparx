ac94ac562812a71f22d5211b3382d9373916fcfa
MohitKS5 <mohitkumarsingh907@gmail.com>
2018-05-25 12:13:44 +0530

    add common utilites and bitparx config

---
+++ common/config/config.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"github.com/bitparx/clientapi/auth/authtypes"
+)
+
+type Bitparx struct {
+	// The configuration required for a server.
+	Bitparx_Server struct {
+		// The name of the server. This is usually the domain name, e.g 'bitpsrx.org', 'localhost'.
+		ServerName string `yaml:"server_name"`
+		// If set, allows registration by anyone who also has the shared
+		// secret, even if registration is otherwise disabled.
+		RegistrationSharedSecret string `yaml:"registration_shared_secret"`
+		// This Home Server's ReCAPTCHA public key.
+		//RecaptchaPublicKey string `yaml:"recaptcha_public_key"`
+		//// This Home Server's ReCAPTCHA private key.
+		//RecaptchaPrivateKey string `yaml:"recaptcha_private_key"`
+		//// Boolean stating whether catpcha registration is enabled
+		//// and required
+		//RecaptchaEnabled bool `yaml:"enable_registration_captcha"`
+		//// Secret used to bypass the captcha registration entirely
+		//RecaptchaBypassSecret string `yaml:"captcha_bypass_secret"`
+		//// HTTP API endpoint used to verify whether the captcha response
+		//// was successful
+		//RecaptchaSiteVerifyAPI string `yaml:"recaptcha_siteverify_api"`
+		//// If set disables new users from registering (except via shared
+		//// secrets)
+		RegistrationDisabled bool `yaml:"registration_disabled"`
+	} `yaml:"bitparx"`
+
+	// Any information derived from the configuration options for later use.
+	Derived struct {
+		Registration struct {
+			// Flows is a slice of flows, which represent one possible way that the client can authenticate a request.
+			// As long as the generated flows only rely on config file options,
+			// we can generate them on startup and store them until needed
+			Flows []authtypes.Flow `json:"flows"`
+
+			// Params that need to be returned to the client during
+			// registration in order to complete registration stages.
+			Params map[string]interface{} `json:"params"`
+		}
+	}
+}
+
+func LoadConfig() *Bitparx {
+	var allowedFlows = []authtypes.Flow{
+		{
+			[]authtypes.LoginType{authtypes.LoginTypeDummy},
+		},
+	}
+	var bitparx Bitparx
+	bitparx.Derived.Registration.Flows = allowedFlows
+	return &bitparx
+}
--- common/storage/storage.go
+++ common/storage/storage.go
@@ -1,24 +1,33 @@
 package storage
 
 import (
-"database/sql"
-"fmt"
-_ "github.com/lib/pq"
+	"fmt"
+	_ "github.com/lib/pq"
+	"github.com/bitparx/clientapi/auth/storage/accounts"
+	"github.com/bitparx/clientapi/auth/storage/devices"
 )
 
 const (
-	DB_USER = "postgres"
+	HOST        = "localhost"
+	PORT        = 5433
+	DB_USER     = "postgres"
 	DB_PASSWORD = "bitparx"
-	DB_NAME = "bitparx"
+	DB_NAME     = "bitparx"
+	SERVER_NAME = "PostgreSQL 10 (x86)"
 )
 
-func PostgresConnect() *sql.DB {
-	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s port=**** sslmode=disable",
-		DB_USER, DB_PASSWORD, DB_NAME)
-	db, err := sql.Open("postgres", dbinfo)
+func PostgresConnect() (accountDB *accounts.Database, deviceDB *devices.Database) {
+	dbinfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		HOST, PORT, DB_USER, DB_PASSWORD, DB_NAME)
+	accountDB, err := accounts.NewDatabase(dbinfo, SERVER_NAME)
 	if err != nil {
 		panic(err)
 	}
-	// defer db.Close()
-	return db
+	deviceDB, err = devices.NewDatabase(dbinfo, SERVER_NAME)
+	if err != nil {
+		panic(err)
+	}
+
+	//defer db.db.Close()
+	return
 }

c58b04b22e8e4555404b0ca88a044d54d4c73a1f
MohitKS5 <mohitkumarsingh907@gmail.com>
2018-05-25 12:14:53 +0530

    utilities used

---
+++ util/util.go
@@ -0,0 +1,56 @@
+package util
+
+import (
+	"net/http"
+	"time"
+	"math/rand"
+)
+
+// JSONResponse represents an HTTP response which contains a JSON body.
+type JSONResponse struct {
+	// HTTP status code.
+	Code int
+	// JSON represents the JSON that should be serialized and sent to the client
+	JSON interface{}
+	// Headers represent any headers that should be sent to the client
+	Headers map[string]string
+}
+
+
+// MessageResponse returns a JSONResponse with a 'message' key containing the given text.
+func MessageResponse(code int, msg string) JSONResponse {
+	return JSONResponse{
+		Code: code,
+		JSON: struct {
+			Message string `json:"message"`
+		}{msg},
+	}
+}
+
+// ErrorResponse returns an HTTP 500 JSONResponse with the stringified form of the given error.
+func ErrorResponse(err error) JSONResponse {
+	return MessageResponse(500, err.Error())
+}
+
+// SetCORSHeaders sets unrestricted origin Access-Control headers on the response writer
+func SetCORSHeaders(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+}
+
+
+const alphanumerics = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+// RandomString generates a pseudo-random string of length n.
+func RandomString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = alphanumerics[rand.Int63()%int64(len(alphanumerics))]
+	}
+	return string(b)
+}
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}

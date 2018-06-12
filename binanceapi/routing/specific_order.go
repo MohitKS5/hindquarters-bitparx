49085722735156643694e8803fd3170e6463720a
MohitKS5 <mohitkumarsingh907@gmail.com>
2018-06-12 11:57:40 +0530

    specific order id

---
+++ binanceapi/routing/specific_order.go
@@ -0,0 +1,31 @@
+package routing
+
+import (
+	ap "github.com/bitparx/binanceapi/auth/authParams"
+	"net/http"
+	rt "github.com/bitparx/binanceapi/rest_api/response_types"
+	"log"
+	"encoding/json"
+)
+
+func GetOrderById(orderID int64, symbol string) (res *rt.Order,err error) {
+	query := map[string]string{
+		"orderId": string(orderID),
+		"symbol": symbol,
+	}
+	req,err := ap.NewRequestWithSignature(BASE_URL+"/api/v3/order",http.MethodGet, query)
+	if err!=nil{
+		return
+	}
+
+	client:=&http.Client{}
+	resp,err:=client.Do(req)
+	if err!=nil || resp.StatusCode != http.StatusOK{
+		log.Println("error: ",err, "\nresp",resp.Status)
+		return
+	}
+
+	res = new(rt.Order)
+	err = json.NewDecoder(resp.Body).Decode(res)
+	return
+}
